f48f2d8841c387c053e308e17afdee4163cd2c17
NurKeinNeid <mralexman3000@gmail.com>
2018-11-15 03:11:50 +0100

    PackageListAdapter: AOSiP -> DU

--- src/com/dirtyunicorns/tweaks/preferences/PackageListAdapter.java
+++ src/com/dirtyunicorns/tweaks/preferences/PackageListAdapter.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.aosip.owlsnest.preference;
+package com.dirtyunicorns.support.preferences.PackageListAdapter;
 
 import android.content.Context;
 import android.content.Intent;
